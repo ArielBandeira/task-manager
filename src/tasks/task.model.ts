8f4fe1c11708352e87308cfd6dea45b11995702a
arielbandeira <arieltwain@gmail.com>
2020-12-03 10:21:17 -0300

    initial commit until lesson 19

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,13 @@
+
+export interface Task {
+    id: string;
+    title: string;
+    description: string;
+    status: TaskStatus;
+}
+
+export enum TaskStatus {
+    OPEN = 'OPEN',
+    IN_PROGRESS = 'IM PROGRESS',
+    DONE = 'DONE'
+}
